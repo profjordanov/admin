2ebf5a29c6b032a7b2e4e8b3b8972231388d1afd
Jordan Jordanov <d_yugioh@abv.bg>
2020-01-30 01:58:31 +0200

    init

--- 01. JS Fundamentals/04 area.js
+++ 01. JS Fundamentals/04 area.js
@@ -4,7 +4,7 @@ function figureArea([w, h, W, H]) {
     let commonArea = Math.min(Number(w), Number(W)) * Math.min(Number(H), Number(h));
 
     let figureArea = fig1Area + fig2Area - commonArea;
-    return figureArea;
+    console.log(figureArea);
 }
 
 figureArea(['2', '4', '5', '3']);
--- 01. JS Fundamentals/05 year.js
+++ 01. JS Fundamentals/05 year.js
@@ -1,10 +1,13 @@
 function leapYear(year) {
+	
 	let condOne = year % 4 == 0 && year % 100 != 0;
 	let condTwo = year % 400 == 0;
+	
     if(condOne || condTwo) {
         console.log("yes");
 		return;
     }
+	
     console.log("no");
 }
 
