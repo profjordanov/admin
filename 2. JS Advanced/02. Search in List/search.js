af3e860f2c075c338c5151b259b4e9e6b1c04d0c
Jordan Jordanov <d_yugioh@abv.bg>
2020-02-01 02:00:41 +0200

    init

--- 2. JS Advanced/02. Search in List/search.js
+++ 2. JS Advanced/02. Search in List/search.js
@@ -2,7 +2,9 @@ function search() {
     let text = $('#searchText').val();
     let matches = 0;
 
-    for (let town of $('#towns li').toArray()) {
+    let towns = $('#towns li').toArray();
+
+    for (let town of towns) {
         if (town.textContent.indexOf(text) != -1) {
             matches++;
             $(town).css('font-weight', 'bold');
