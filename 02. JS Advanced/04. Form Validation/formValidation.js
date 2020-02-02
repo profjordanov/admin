eb8c1fd9092995ee33925f539fefa668cb1cc345
Jordan Jordanov <d_yugioh@abv.bg>
2020-02-02 16:01:55 +0200

    init

--- 02. JS Advanced/04. Form Validation/formValidation.js
+++ 02. JS Advanced/04. Form Validation/formValidation.js
@@ -1,7 +1,7 @@
 function validate() {
     $('#company').on('change', showHideCompany);
 
-    $('#submit').on('click', function (ev) {
+    $('#submit').on('click', function(ev) {
         ev.preventDefault();
         let usernameRegex = /^[a-zA-Z0-9]{3,20}$/;
         let passwordRegex = /^\w{5,15}$/;
@@ -10,36 +10,36 @@ function validate() {
 
         let allFieldsValid = true;
 
-        if($('#username').val().match(usernameRegex)){
+        if ($('#username').val().match(usernameRegex)) {
             $('#username').css('border', 'none');
         } else {
             $('#username').css('border-color', 'red');
             allFieldsValid = false;
         }
 
-        if($('#password').val().match(passwordRegex)){
+        if ($('#password').val().match(passwordRegex)) {
             $('#password').css('border', 'none');
         } else {
             $('#password').css('border-color', 'red');
             allFieldsValid = false;
         }
 
-        if($('#email').val().match(emailRegex)){
+        if ($('#email').val().match(emailRegex)) {
             $('#email').css('border', 'none');
         } else {
             $('#email').css('border-color', 'red');
             allFieldsValid = false;
         }
 
-        if($('#confirm-password').val().match(passwordRegex) && $('#confirm-password').val().localeCompare($('#password').val()) == 0){
+        if ($('#confirm-password').val().match(passwordRegex) && $('#confirm-password').val().localeCompare($('#password').val()) == 0) {
             $('#confirm-password').css('border', 'none');
         } else {
             $('#confirm-password').css('border-color', 'red');
             allFieldsValid = false;
         }
 
-        if($('#company').is(':checked')){
-            if($('#companyNumber').val().match(companyNumberRegex)){
+        if ($('#company').is(':checked')) {
+            if ($('#companyNumber').val().match(companyNumberRegex)) {
                 $('#companyNumber').css('border', 'none');
             } else {
                 $('#companyNumber').css('border-color', 'red');
@@ -47,7 +47,7 @@ function validate() {
             }
         }
 
-        if(allFieldsValid){
+        if (allFieldsValid) {
             $('#valid').css('display', 'block');
         } else {
             $('#valid').css('display', 'none');
@@ -56,10 +56,10 @@ function validate() {
     });
 
     function showHideCompany() {
-            if($(this).is(':checked')){
-                $('#companyInfo').css('display', 'block');
-            } else {
-                $('#companyInfo').css('display', 'none')
-            }
+        if ($(this).is(':checked')) {
+            $('#companyInfo').css('display', 'block');
+        } else {
+            $('#companyInfo').css('display', 'none')
+        }
     }
 }
