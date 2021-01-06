5ebd2eec45875ec8b444e90aaeafe6cb058bfa68
Jordan Jordanov <d_yugioh@abv.bg>
2021-01-06 08:06:33 +0200

    Create examination-insert.sql

---
+++ sql/examination-insert.sql
@@ -0,0 +1,14 @@
+USE [StudentsManagerDb]
+GO
+
+INSERT INTO [dbo].[CourseExaminations]
+           ([Type]
+           ,[ResourceUrl]
+           ,[ExtraResourceUrl])
+     VALUES
+           ('First',
+           'https://github.com/profjordanov/storage/blob/main/variantB.pdf',
+           'https://github.com/profjordanov/storage/raw/main/variantB.zip')
+GO
+
+
