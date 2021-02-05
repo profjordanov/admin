92597fba7b7704630844d9a24004b98762b2dfc1
Yordan <d_yugioh@abv.bg>
2021-02-05 10:35:28 +0200

    footer

---
+++ globo-app/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import * as RootNavigation from './RootNavigation';
+
+export default function Footer(){
+    return (
+        <View style={styles.footer}>
+
+        </View>
+    )
+}
